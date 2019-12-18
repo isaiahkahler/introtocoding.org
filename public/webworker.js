e4e5e2f5fee461f1326761447ae21be8bbc8415c
Isaiah Kahler <isaiahwkahler@gmail.com>
2019-12-18 12:49:35 -0500

    working monaco editor + basic skulpt

---
+++ public/webworker.js
@@ -0,0 +1,33 @@
+importScripts('./skulpt.min.js');
+importScripts('./skulpt-stdlib.js');
+
+console.log('web worker started');
+
+this.addEventListener('message', (message) => {
+
+    const code = message.data;
+    Sk.configure({
+        output: (output) => {
+            postMessage(output);
+        },
+        read: (x) => {
+            if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
+                throw "File not found: '" + x + "'";
+            return Sk.builtinFiles["files"][x];
+        }
+    });
+
+    var myPromise = Sk.misceval.asyncToPromise(function () {
+        return Sk.importMainWithBody("<stdin>", false, code, true);
+    });
+    myPromise.then(function (mod) {
+        // console.log('success');
+    },
+        function (err) {
+            console.log(err.toString());
+            postMessage(err.toString())
+        });
+
+
+});
+
--- src/pages/learning/lesson/lesson.tsx
+++ src/pages/learning/lesson/lesson.tsx
@@ -1,13 +1,50 @@
-import React, { useEffect, useState, useMemo } from 'react';
+import React, { useEffect, useState, useMemo, useRef } from 'react';
 import Page from '../../../components/page';
 import Editor from '@monaco-editor/react';
-
+import { Button } from '@material-ui/core';
 
 function LessonContainer(props: {}) {
    
+    // const [loadedSkulpt, setLoadedSkulpt] = useState(0);
+    const valueGetter = useRef<() => string>();
+
+    const [editorReady, setEditorReady] = useState(false);
+
+    const [skulptWorker, setSkulptWorker] = useState<Worker>();
+
+    const [output, setOutput] = useState('');
+
+    // load skulpt worker
+    useEffect(() => {
+        setSkulptWorker(new Worker('./webworker.js'))
+    }, []);
+
+    // set skulpt listener
+    useEffect(() => {
+        if(!!skulptWorker){
+            skulptWorker.addEventListener('message', (message) => {
+                setOutput(previous => previous + message.data);
+            });
+        }
+    }, [skulptWorker]);
+
     return (
         <Page title='Learning!'>
-            <Editor language='python' height='500px' />
+            <Editor language='python' height='500px' editorDidMount={(_valueGetter) => {
+                setEditorReady(true);
+                valueGetter.current = _valueGetter;
+            }} />
+
+            <Button variant='contained' onClick={() => {
+                !!valueGetter.current && !!skulptWorker && skulptWorker.postMessage(valueGetter.current());
+                setOutput('');
+                }} >
+                    RUn
+                </Button>
+
+            <div>
+                {output}
+            </div>
         </Page>
     );
 }
