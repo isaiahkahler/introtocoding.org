100ce89f73e397a3309276dd5e57402401b9af8d
Isaiah Kahler <isaiahwkahler@gmail.com>
2020-01-14 09:05:23 -0500

    mui grid -> custom grid

--- src/components/environment/environment.tsx
+++ src/components/environment/environment.tsx
@@ -1,23 +1,43 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, DetailedHTMLProps, HTMLAttributes, PropsWithChildren } from 'react';
 import Sidebar from './sidebar';
 import Terminal from './terminal';
 import Editor from './editor';
 import { useSkulpt } from './skulpt';
 
 import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, styled } from '@material-ui/core/styles';
 import { Button } from '@material-ui/core';
+
+
 
 interface EnvironmentProps {
 
 }
 
+const StyledEnvironment = styled('div')(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'row',
+}));
 
-const useStyles = makeStyles(theme => ({
-    sidebar: {
+const StyledSidebar = styled('div')(({ theme }) => ({
+    width: '250px',
+    [theme.breakpoints.down('xs')]: {
+        width: '100vw',
+    },
+}));
 
-    }
-  }));
+
+const StyledColumn = (props: PropsWithChildren<{ vertical: boolean }>) => {
+    return (
+        <div style={{
+            display: 'flex',
+            flexDirection: props.vertical ? 'column' : 'row',
+            width: '100%',
+        }}>
+            {props.children}
+        </div>
+    );
+}
 
 export default function Environment(props: EnvironmentProps) {
 
@@ -25,28 +45,35 @@ export default function Environment(props: EnvironmentProps) {
 
     const [showTerminal, setShowTerminal] = useState(true);
 
+    const [terminalVertical, setTerminalVertical] = useState(true);
+
     const editorValueGetter = useRef<() => string>();
 
-    const runCode = useSkulpt((output) => {console.log(output)});
+    const runCode = useSkulpt((output) => { console.log(output) });
 
-    return(
+    const [count, setCount] = useState(1);
+
+
+
+    return (
 
         //implement button bar
 
-        <Grid container>
-            <Grid item md={3}>sidebar</Grid>
-            <Grid item container xs>
-                <Grid item xs={12}>
-                    <Editor theme='dark' onEditorReady={() => {}} valueGetter={editorValueGetter} /> 
-                </Grid>
-                <Grid item xs={12}>
-                    <Terminal />
-                    {/* temporary */}
-                    <Button onClick={() => {
-                        !!editorValueGetter && editorValueGetter.current && runCode(editorValueGetter.current());
-                    }} variant='contained'>run</Button>
-                </Grid>
-            </Grid>
-        </Grid>
+        <StyledEnvironment>
+            {showSidebar && <StyledSidebar>
+                sidebar
+                <Button variant='contained' onClick={() => { setCount(count + 1) }}>
+                    {count} clicks
+                </Button>
+            </StyledSidebar>}
+            <StyledColumn vertical={terminalVertical}>
+                <Editor theme='dark' onEditorReady={() => { }} valueGetter={editorValueGetter} />
+                {showTerminal && <div>
+                    {/* terminal */}
+                    {/* <Button onClick={() => { setTerminalVertical(!terminalVertical) }}>switch</Button> */}
+                    <Terminal onInput={(input) => {console.log(input)}} theme='light' />
+                </div>}
+            </StyledColumn>
+        </StyledEnvironment>
     );
 }
