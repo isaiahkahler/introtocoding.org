5cb0533bde681ebe46cd9ba3b7d3fd27f17f6120
Isaiah Kahler <isaiahwkahler@gmail.com>
2020-01-29 09:24:11 -0500

    toolbar spacer mixin

---
+++ src/components/mixins.tsx
@@ -0,0 +1,9 @@
+import {styled} from '@material-ui/core/styles';
+
+export const ToolbarSpacer = styled('div')(({theme}) => ({
+    ...theme.mixins.toolbar,
+    paddingTop: theme.spacing(1),
+    [theme.breakpoints.down('sm')]: {
+        paddingTop: 0
+    }
+}));
