ddb134a0409ad357f435779187e35f91ab9eba60
Isaiah Kahler <isaiahwkahler@gmail.com>
2020-02-25 23:53:16 -0500

    fixed toolbar padding

--- src/components/mixins.tsx
+++ src/components/mixins.tsx
@@ -2,8 +2,8 @@ import {styled} from '@material-ui/core/styles';
 
 export const ToolbarSpacer = styled('div')(({theme}) => ({
     ...theme.mixins.toolbar,
-    paddingTop: theme.spacing(1),
-    [theme.breakpoints.down('sm')]: {
-        paddingTop: 0
-    }
+    // paddingTop: theme.spacing(1),
+    // [theme.breakpoints.down('sm')]: {
+    //     paddingTop: 0
+    // }
 }));
