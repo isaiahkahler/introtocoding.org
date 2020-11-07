8237c327ccbc49605a684cbfa345aa854f4ef18b
Isaiah Kahler <isaiahkahler@0587392728.wireless.umich.net>
2020-11-06 21:07:43 -0500

    landing page

---
+++ public/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {  
+
+    let typed = new Typed('#typed', {
+      stringsElement: '#typed-strings',
+      typeSpeed: 60,
+    });
+  });
--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -3,10 +3,15 @@ import { Link } from 'react-router-dom';
 
 function LandingContainer(props: {}) {
     return (
-        <div>
-            <h1>WORK IN PROGRESS!</h1>
-            <h2>this web application is currently construction. </h2>
-            <Link to='/home'><a><h2>continue...</h2></a></Link>
+        // <div>
+        //     <h1>WORK IN PROGRESS!</h1>
+        //     <h2>this web application is currently construction. </h2>
+        //     <Link to='/home'><a><h2>continue...</h2></a></Link>
+        // </div>
+        <div style={{minHeight: "100vh", minWidth: "100vw", position: "absolute", left: 0, right: 0, top: 0, bottom: 0}}>
+            <iframe src="./test.html" width="100%" height="100%" frameBorder={0}>
+
+            </iframe>
         </div>
     );
 }
